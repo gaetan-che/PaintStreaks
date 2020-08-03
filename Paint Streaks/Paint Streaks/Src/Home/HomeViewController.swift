b8302f5003748f779b947bf2767a23e8807417b2
Gaëtan Chevalley <gaetan.chevalley@abonobo.com>
2020-08-03 18:01:16 +0200

    chore: initial project setup

---
+++ Paint Streaks/Paint Streaks/Src/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Paint Streaks
+//
+//  Created by Gaëtan Chevalley on 03.08.20.
+//  Copyright © 2020 Paint Streaks. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
